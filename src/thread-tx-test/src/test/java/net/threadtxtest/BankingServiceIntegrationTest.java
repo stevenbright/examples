cdad924bfcaa4ac7acc0317f93e2fedc1b63ffb7
Alexander Shabanov <avshabanov@gmail.com>
2011-08-15 21:16:43 +0400

    Adding testRegisterUser.

--- src/thread-tx-test/src/test/java/net/threadtxtest/BankingServiceIntegrationTest.java
+++ src/thread-tx-test/src/test/java/net/threadtxtest/BankingServiceIntegrationTest.java
@@ -1,10 +1,13 @@
 package net.threadtxtest;
 
+import net.threadtxtest.service.BankingService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
 
 import static org.junit.Assert.*;
 
@@ -16,11 +19,27 @@ import static org.junit.Assert.*;
 @Transactional
 public class BankingServiceIntegrationTest {
 
+    @Resource
+    private BankingService bankingService;
+
     /**
      * Sample test method.
      */
     @Test
     public void testDummy() {
         assertTrue(true);
+    }
+
+    @Test
+    public void testRegisterUser() {
+        final long ids[] = {
+                bankingService.registerUser("alex"),
+                bankingService.registerUser("bob"),
+                bankingService.registerUser("dave")
+        };
+
+        assertNotSame(ids[0], ids[1]);
+        assertNotSame(ids[0], ids[2]);
+        assertNotSame(ids[1], ids[2]);
     }
 }
