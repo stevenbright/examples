7e732eb9304b9cce39c46dac7b0764b9d34a706a
Steven S <vstrength@gmail.com>
2011-09-10 17:40:01 +0400

    sample bridge code

--- src/clojure/aspm-clj/src/main/clojure/org/aspm/bridge.clj
+++ src/clojure/aspm-clj/src/main/clojure/org/aspm/bridge.clj
@@ -4,3 +4,34 @@
   (println "=== ASPM Clojure Bridge ===")
   (println "Java main called clojure function with args: "
     (apply str (interpose " " args))))
+
+;;(main '(1 2 3 4))
+
+(defn dummy []
+  '(
+     ;; define AST expression capture rules
+     (defrule
+       ;; method name pattern
+       (action entity (:opt quantifier))
+       ;; method signature
+       (arguments => type)
+       ;; action on pattern
+       (foo))
+
+     nil)
+
+  ;; define concrete rules
+  '(
+     ;; save rule
+     (
+     ;; method name pattern
+     (:save entity)
+     ;; signature pattern
+     (arguments => type-id)
+     ;; rules
+
+     nil)
+
+  nil)
+
+  nil)
