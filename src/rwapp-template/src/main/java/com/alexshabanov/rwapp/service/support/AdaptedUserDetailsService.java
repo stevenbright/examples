f59c75fc9025751713cf2bd6bb5629cbb1d47480
Steven S <vstrength@gmail.com>
2012-02-01 12:58:22 +0400

    Optimizing imports.

--- src/rwapp-template/src/main/java/com/alexshabanov/rwapp/service/support/AdaptedUserDetailsService.java
+++ src/rwapp-template/src/main/java/com/alexshabanov/rwapp/service/support/AdaptedUserDetailsService.java
@@ -7,7 +7,6 @@ import com.alexshabanov.rwapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -15,7 +14,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
