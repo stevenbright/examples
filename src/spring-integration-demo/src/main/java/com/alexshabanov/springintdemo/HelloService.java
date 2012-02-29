88d0cbdc7609e01735deaa26f09986f4c8c2c929
Alexander Shabanov <avshabanov@gmail.com>
2012-02-29 12:20:26 +0400

    Adding spring integration demo.

---
+++ src/spring-integration-demo/src/main/java/com/alexshabanov/springintdemo/HelloService.java
@@ -0,0 +1,17 @@
+package com.alexshabanov.springintdemo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Represents sample service.
+ */
+public final class HelloService {
+    private static final Logger LOG = LoggerFactory.getLogger(HelloService.class);
+
+    public int add(int a, int b) {
+        LOG.info("adding {} + {}", a, b);
+        return a + b;
+    }
+}
