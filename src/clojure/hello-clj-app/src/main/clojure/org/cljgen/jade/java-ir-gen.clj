0a20524fb5b96272163178f31b63dc95aca208ff
Alexander Shabanov <avshabanov@gmail.com>
2012-03-27 01:21:42 +0400

    jjf

--- src/clojure/hello-clj-app/src/main/clojure/org/cljgen/jade/java-ir-gen.clj
+++ src/clojure/hello-clj-app/src/main/clojure/org/cljgen/jade/java-ir-gen.clj
@@ -14,7 +14,7 @@
 ;;
 
 
-#_(let [path-base "/home/steve/projects/java-maven-tests/src/clojure/hello-clj-app/src/main/clojure/org/cljgen/jade/"
+#_(let [path-base "/Users/alex/proj/java-maven-tests/src/clojure/hello-clj-app/src/main/clojure/org/cljgen/jade/"
         path-base-0 "/home/steve/projects/java-maven-tests/src/clojure/hello-clj-app/src/main/clojure/org/cljgen/jade/"
         path-base-1 "/Users/alex/proj/java-maven-tests/src/clojure/hello-clj-app/src/main/clojure/org/cljgen/jade/"]
     (load-file (str path-base "util.clj"))
@@ -78,6 +78,14 @@
                          (= form \tab) (fail "Tabs are auto-generated hence unexpected in " more)
                          :else (print form))
           (string? form) (do (align-and-print \") (print form) (print \"))
+          (map? form) (do
+                        (if @add-space (align-and-print pprinter-whitespace))
+                        (align-and-print "")
+                        (pprint-ir (form :value)))
+          (coll? form) (do
+                         (if @add-space (align-and-print pprinter-whitespace))
+                         (align-and-print "")
+                         (apply pprint-ir form))
           (nil? form) nil ; do nothing
           :else (fail "Unknown form " form " in " more))))))
 
@@ -111,31 +119,70 @@
 
 (def jif-default-order-value 10)
 
-(defn jif-ordered-fields [dto-form]
+(defn jif-ordered-fields [fields]
   (letfn [(order-of [field] (let [field-sig (second field)]
                               (cond
                                 (keyword? field-sig) jif-default-order-value
                                 (map? field-sig) (get field-sig :order jif-default-order-value)
                                 :else (fail "Unknown field " field " signature: " field-sig))))]
-    (sort (fn [l r] (- (order-of l) (order-of r))) (dto-form :fields))))
+    (sort (fn [l r] (- (order-of l) (order-of r))) fields)))
 
+(defn jif-type-of [field]
+  (let [field-type (second field)]
+    (cond
+      (keyword? field-type) field-type
+      (map? field-type) (field-type :type)
+      :else (fail "Unknown type record in field " field))))
 
+(defn jif-produce-dto-ir [dto-form]
+  (let [fields (dto-form :fields)
+        class-name {:value (dto-form :name)}]
+    (lazy-cat
+      [:public :final :class class-name \{ \newline]
+      ;; fields
+      (map
+        (fn [field] [:private :final (jif-type-of field) (first field) \; \newline])
+        (jif-ordered-fields fields))
 
+      [\newline]
+
+      ;; constructor
+      [:public class-name \(
+       (interpose \, (map (fn [field] [(jif-type-of field) (first field)]) fields))
+       \) \{
+       (map (fn [field] [:this \. (first field) \= (first field) \; \newline]) fields)
+       \}]
+
+      [\newline]
+
+      ;; getters
+      (map (fn [field] [:public (as-camel-name "get" (name (jif-type-of field))) \newline]) fields)
+
+      [\}])))
+
+#_(let [dto-form {:name :Person
+                  :fields {
+                            :id {:type :Long :order 0}
+                            :fullName :String
+                            :age :int
+                            }}]
+    ;;(doall (map (fn [x] (println "x =" x)) (vals (dto-form :fields))))
+    (apply pprint-ir (jif-produce-dto-ir dto-form)))
 
 ;;
 ;; =====================================================================================================================
 ;;
 
 #_(dosync
-    (let [dto-form {:name "Person"
+    (let [dto-form {:name :Person
                     :fields {
-                              "id" {:type :Long :order 0}
-                              "fullName" :String
-                              "age" :int
+                              :id {:type :Long :order 0}
+                              :fullName :String
+                              :age :int
                               }
                     }]
       (println dto-form)
-      (println "ordered = " (jif-ordered-fields dto-form))
+      (println "ordered = " (jif-ordered-fields (dto-form :fields)))
       (doseq [f (dto-form :fields)]
         (println "  f = " f))))
 
