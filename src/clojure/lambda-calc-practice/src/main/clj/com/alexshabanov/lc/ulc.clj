4c575f0507042701779ea8814a3b0ea7af229c47
Alexander Shabanov <avshabanov@gmail.com>
2012-06-29 19:11:12 +0400

    tbd

--- src/clojure/lambda-calc-practice/src/main/clj/com/alexshabanov/lc/ulc.clj
+++ src/clojure/lambda-calc-practice/src/main/clj/com/alexshabanov/lc/ulc.clj
@@ -1,10 +1,18 @@
 (ns com.alexshabanov.lc)
 
-(defn tru [u v] u)
-(defn fls [u v] v)
-
 (defn u-or [u v] (u tru v))
 
 
+
+(def globals (atom {}))
+
+
+(defmacro deflambda [name args body])
+
+(defn tru [u v] u)
+(defn fls [u v] v)
+
+
 (defn main [args]
+  (reset! globals {:a 1})
   (println "args = " args))
