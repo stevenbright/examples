0e10a2f50931be54c3e069930d9a09b0abf0b215
Alexander Shabanov <avshabanov@gmail.com>
2012-11-22 11:47:05 +0400

    Adding sample gradle app

---
+++ src/learn-gradle/first-gradle-app/src/main/java/com/alexhsabanov/sample/gradleapp/App.java
@@ -0,0 +1,10 @@
+package com.alexhsabanov.sample.gradleapp;
+
+/**
+ * Entry point.
+ */
+public final class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
