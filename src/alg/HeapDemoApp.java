06ce3a4e1603125fab460f82ee84c7f53fe09d47
Alexander Shabanov <avshabanov@gmail.com>
2013-03-10 20:22:49 +0400

    Heap demo now illustrates absolutely correct insertion and removal to/from heap what is additionaly checked by set

--- src/alg/HeapDemoApp.java
+++ src/alg/HeapDemoApp.java
@@ -2,11 +2,12 @@ import java.util.*;
 
 
 public final class HeapDemoApp {
-    final Random random = new Random(32);
 
-    void run() {
-        // fill arr
+    public static void main(String[] args) {
         final int size = 15;
+        final Random random = new Random(32);
+
+        // fill heap and set which will later be used to validate heap contents
         final IntHeap heap = new IntHeap();
         final Set<Long> set = new LinkedHashSet<Long>(size);
 
@@ -16,34 +17,29 @@ public final class HeapDemoApp {
                 element = random.nextInt(100);
             } while (heap.contains(element));
 
-            System.out.println("Adding " + element);
+            System.out.println("Adding " + element + ", heap array:");
             heap.add(element);
-            System.out.println("Heap array: " + heap.asList());
+            System.out.println("\t\t" + heap.asList());
             heap.validate();
 
             set.add(element);
         }
 
-        System.out.println("List of elements in order of appearance:\n\t" + set);
+        System.out.println("List of elements in order of appearance:\n\t\t" + set);
+        System.out.println("\n=== Consecutive root removal ===\n");
 
-        // TODO: fix removal
-
-        System.out.println("---\nRoot removal:\n");
         while (!heap.isEmpty()) {
             final long element = heap.removeRoot();
             System.out.println("Heap array: " + heap.asList());
             heap.validate();
 
-            set.remove(set.iterator().next());
+            // validate against previously constructed set minus root element
+            set.remove(element);
             if (!set.containsAll(heap.asList())) {
                 set.removeAll(heap.asList());
                 throw new AssertionError("Element " + element + " is not properly deleted, diff: " + set);
             }
         }
-    }
-
-    public static void main(String[] args) {
-        new HeapDemoApp().run();
     }
 }
 
@@ -91,23 +87,25 @@ final class IntHeap {
         int last = 0;
         final long element = arr[last];
 
-        // copy last one
+        // copy last one which is now the root candidate
         arr[last] = arr[arr.length - 1];
 
-        // copy array
+        // shrink array
         final long[] newArr = new long[arr.length - 1];
-        System.arraycopy(arr, 1, newArr, 0, arr.length - 1);
+        System.arraycopy(arr, 0, newArr, 0, arr.length - 1);
         arr = newArr;
 
-        // rebuild structure
-        for (;;) {
-            int i = swapWithChild(last);
-            System.out.println("swap " + i + ", " + last);
-            if (i == last) {
-                break;
-            }
+        if (arr.length > 0) {
+            // rebuild heap structure so it will become max heap again
+            for (;;) {
+                int i = swapWithChild(last);
+                System.out.println("swap " + i + ", " + last);
+                if (i == last) {
+                    break;
+                }
 
-            last = i;
+                last = i;
+            }
         }
 
         return element;
@@ -119,10 +117,12 @@ final class IntHeap {
                 return true;
             }
         }
+
         return false;
     }
 
     public List<Long> asList() {
+        // guava, where art thou?
         final List<Long> list = new ArrayList<Long>(arr.length);
         for (final long element : arr) {
             list.add(element);
@@ -176,13 +176,14 @@ final class IntHeap {
         int largest = index;
 
         if (right < arr.length) {
-            if (arr[left] > arr[right]) {
+            if (arr[right] > arr[left]) {
                 largest = right;
             } else {
                 largest = left;
             }
 
             if (arr[index] > arr[largest]) {
+                // root is greater than largest element
                 largest = index;
             }
         } else if (left < arr.length) {
