a63905e7ab624bf6278d7a19e4ede1b278e86be7
Alexander Shabanov <avshabanov@gmail.com>
2013-08-14 18:53:35 +0400

    PN2

--- src/math/zeta/primes.lisp
+++ src/math/zeta/primes.lisp
@@ -15,7 +15,7 @@
 
 (defun count-of-primes (from to)
   "Finds count of primes that are located between the given
-fix number (i.e. integer) arguments.
+fix number (i.e. integer) arguments; from inclusive, to exclusive.
 Uses 'Sieve of Erathosphen' algorithm adapted to common lisp"
   (declare (type fixnum from) (type fixnum to))
   (assert (>= to from) (to from)
@@ -37,6 +37,42 @@ Uses 'Sieve of Erathosphen' algorithm adapted to common lisp"
     
     ;; return the result
     (values count primes)))
+
+(defun produce-primes (from to)
+  (multiple-value-bind (count primes-bitset) (count-of-primes from to)
+    (let ((result (make-array count :element-type 'number)))
+      (loop for i from from to (- to 1) with j = 0 do
+           (if (= 0 (aref primes-bitset i))
+               (progn
+                 (setf (aref result j) i)
+                 (incf j))))
+      result)))
+
+#+repl (produce-primes 2 10)
+#+repl (produce-primes 2 11)
+#+repl (produce-primes 0 12)
+#+repl (produce-primes 1990 2020)
+
+;; Memory-friendly version of count-of-primes
+#+repl (defun count-of-primes2 (from to)
+  (declare (type number from) (type number to))
+  (assert (>= to from) (to from)
+          "`to'=~S argument should be greater than `from'=~S argument" to from)
+  (let* ((limit (floor (sqrt to)))
+         (known-primes (produce-primes 0 limit))
+         (count 0))
+
+    (flet ((is-prime (num)
+             (loop for pn being the elements of known-primes do
+                  (if (> pn num) (return-from is-prime nil))
+                  (if (not (= 0 (mod num pn))) (return-from is-prime nil)))
+             t))
+      (loop for i from from to (- to 1) do
+         (if (is-prime i) (incf count))))
+    
+    count))
+
+#+repl (count-of-primes2 0 100)
 
 ;; Test
 #+repl (progn
