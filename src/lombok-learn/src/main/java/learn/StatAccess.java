18cff957606a78274473f25c5d4c92f092afbb26
Alexander Shabanov <avshabanov@gmail.com>
2015-07-15 23:26:03 -0700

    Adding define support

---
+++ src/lombok-learn/src/main/java/learn/StatAccess.java
@@ -0,0 +1,12 @@
+package learn;
+
+/**
+ * @author Alexander Shabanov
+ */
+public class StatAccess {
+  private static Integer sym = new Integer(0);
+
+  public Integer getSym() {
+    return sym;
+  }
+}
