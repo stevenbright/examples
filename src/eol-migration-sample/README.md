cf1e7108e02baabf5b8455b900c740e91e87d6c6
Alexander Shabanov <avshabanov@gmail.com>
2015-08-21 22:39:44 -0700

    [eol-migration-sample] Adding diag queries + lombok model

--- src/eol-migration-sample/README.md
+++ src/eol-migration-sample/README.md
@@ -2,3 +2,32 @@
 # Overview
 
 Demonstrates performing manual SQL migration to the completely new schema.
+
+# H2 cheats
+
+## Initialize
+
+Create a property override file:
+
+```
+eol.dao.dataSource.url=jdbc:h2:/tmp/lairedb
+```
+
+... and use it by referring to it in JVM properties: ``-Dapp.properties.override=file:/Path/to/eol.properties``.
+
+## Import data
+
+```
+java -cp ~/.m2/repository/com/h2database/h2/1.4.183/h2-1.4.183.jar org.h2.tools.RunScript -url jdbc:h2:/tmp/lairedb -user sa -script booklib-website/src/main/resources/sql/h2/book/book-schema.sql
+
+java -cp ~/.m2/repository/com/h2database/h2/1.4.183/h2-1.4.183.jar org.h2.tools.RunScript -url jdbc:h2:/tmp/lairedb -user sa -script booklib-website/src/main/resources/sql/h2/book/book-fixture.sql
+
+java -cp ~/.m2/repository/com/h2database/h2/1.4.183/h2-1.4.183.jar org.h2.tools.RunScript -url jdbc:h2:/tmp/lairedb -user sa -script eolaire-server/src/main/resources/eolaireService/sql/eolaire-schema.sql
+```
+
+## Connect
+
+```
+rlwrap java -cp ~/.m2/repository/com/h2database/h2/1.4.183/h2-1.4.183.jar org.h2.tools.Shell -url jdbc:h2:/tmp/lairedb -user sa
+```
+
--- src/eol-migration-sample/src/main/java/com/alexshabanov/sample/eol/migration/service/MigrationService.java
+++ src/eol-migration-sample/src/main/java/com/alexshabanov/sample/eol/migration/service/MigrationService.java
@@ -1,8 +1,15 @@
 package com.alexshabanov.sample.eol.migration.service;
 
+import com.alexshabanov.sample.eol.migration.model.NamedValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public final class MigrationService {
   public interface Contract {
@@ -11,18 +18,53 @@ public final class MigrationService {
 
   public static final class Impl implements Contract {
     private final Logger log = LoggerFactory.getLogger(getClass());
-    private final JdbcOperations jdbcOperations;
+    private final JdbcOperations db;
 
-    public Impl(JdbcOperations jdbcOperations) {
-      this.jdbcOperations = jdbcOperations;
+    public Impl(JdbcOperations db) {
+      this.db = db;
     }
 
     @Override
+    @Transactional
     public void runMigration() {
+      final String n = new NamedValue(1L, "dfs").getName();
       log.info("About to run migration...");
 
-      final int i = jdbcOperations.queryForObject("SELECT 1", Integer.class);
-      log.info("Got i={}", i);
+      try {
+        final int i = db.queryForObject("SELECT COUNT(0) FROM item", Integer.class);
+        log.info("Got i={}", i);
+      } catch (DataAccessException ignored) {
+        log.warn("There is no items table, schema is invalid, returning");
+        return;
+      }
+
+      // run other diagnostics queries
+      db.queryForObject("SELECT COUNT(0) FROM entity_type", Integer.class);
+
+      // booklib tables
+      db.queryForObject("SELECT COUNT(0) FROM genre", Integer.class);
+      db.queryForObject("SELECT COUNT(0) FROM author", Integer.class);
+      db.queryForObject("SELECT COUNT(0) FROM entity_type", Integer.class);
+      db.queryForObject("SELECT COUNT(0) FROM book_meta", Integer.class);
+
+      //db.query("")
+
+      // TODO: remove, once components will play nicely together
+      if (db.queryForObject("SELECT COUNT(0) FROM book_meta", Integer.class) != 1) {
+        throw new IllegalStateException("Forcing rollback in a hacky way");
+      }
+    }
+  }
+
+  //
+  // Private
+  //
+
+  private static final class GenreRowMapper implements RowMapper<NamedValue> {
+
+    @Override
+    public NamedValue mapRow(ResultSet rs, int rowNum) throws SQLException {
+      return new NamedValue(rs.getLong("id"), rs.getString("name"));
     }
   }
 }
