9bcf79714d6db5aa41e02cf46f4d6b92c2be1a11
Alexander Shabanov <avshabanov@gmail.com>
2016-03-12 13:16:27 -0800

    Working hello grpc sample

---
+++ src/grpc/hello/hello-client/src/main/java/com/alexshabanov/grpc/hello/HelloClient.java
@@ -0,0 +1,94 @@
+package com.alexshabanov.grpc.hello;
+
+import io.grpc.*;
+import io.grpc.protobuf.ProtoUtils;
+import io.grpc.stub.AbstractStub;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+import static io.grpc.stub.ClientCalls.blockingUnaryCall;
+
+/**
+ * @author Alexander Shabanov
+ */
+public final class HelloClient {
+
+  private final Logger log = LoggerFactory.getLogger(getClass());
+  private final ManagedChannel channel;
+  private final HelloServiceGrpc.HelloServiceBlockingClient blockingStub;
+
+  /** Construct client connecting to HelloWorld server at {@code host:port}. */
+  public HelloClient(String host, int port) {
+    channel = ManagedChannelBuilder.forAddress(host, port)
+        .usePlaintext(true) // - for debug only
+        .build();
+    blockingStub = new HelloJsonStub(channel);
+  }
+
+  public void shutdown() throws InterruptedException {
+    channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+  }
+
+  /** Say hello to server. */
+  public void greet(String name) {
+    log.info("Will try to greet {} ...", name);
+    HelloModel.GreetingRequest request = HelloModel.GreetingRequest.newBuilder().setName(name).build();
+    HelloModel.GreetingReply response;
+    try {
+      response = blockingStub.getGreeting(request);
+    } catch (StatusRuntimeException e) {
+      log.warn("RPC failed: {}", e.getStatus());
+      return;
+    }
+    log.info("Greeting: {}", response.getGreeting());
+  }
+
+  public static void main(String[] args) throws InterruptedException {
+    HelloClient client = new HelloClient("localhost", 8081);
+    try {
+      /* Access a service running on the local machine on port 50051 */
+      String user = "world";
+      if (args.length > 0) {
+        user = args[0]; /* Use the arg as the name to greet if provided */
+      }
+      client.greet(user);
+    } finally {
+      client.shutdown();
+    }
+  }
+
+  //
+  // Client impl
+  //
+
+  private static final class HelloJsonStub extends AbstractStub<HelloJsonStub>
+      implements HelloServiceGrpc.HelloServiceBlockingClient {
+
+    static final MethodDescriptor<HelloModel.GreetingRequest, HelloModel.GreetingReply> METHOD_GET_GREETING =
+        MethodDescriptor.create(
+            HelloServiceGrpc.METHOD_GET_GREETING.getType(),
+            HelloServiceGrpc.METHOD_GET_GREETING.getFullMethodName(),
+            ProtoUtils.marshaller(HelloModel.GreetingRequest.getDefaultInstance()),
+            ProtoUtils.marshaller(HelloModel.GreetingReply.getDefaultInstance()));
+
+    protected HelloJsonStub(Channel channel) {
+      super(channel);
+    }
+
+    protected HelloJsonStub(Channel channel, CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @Override
+    protected HelloJsonStub build(Channel channel, CallOptions callOptions) {
+      return new HelloJsonStub(channel, callOptions);
+    }
+
+    @Override
+    public HelloModel.GreetingReply getGreeting(HelloModel.GreetingRequest request) {
+      return blockingUnaryCall(getChannel(), METHOD_GET_GREETING, getCallOptions(), request);
+    }
+  }
+}
--- src/grpc/hello/hello-server/src/main/java/com/alexshabanov/grpc/hello/HelloServer.java
+++ src/grpc/hello/hello-server/src/main/java/com/alexshabanov/grpc/hello/HelloServer.java
@@ -2,31 +2,88 @@ package com.alexshabanov.grpc.hello;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.net.URL;
+import java.util.Date;
 
 /**
+ * HelloServer, a server-side part that exposes HelloService.
+ *
  * @author Alexander Shabanov
  */
 public final class HelloServer {
+  private final Logger log = LoggerFactory.getLogger(getClass());
   private final int port;
   private final Server server;
 
-//  public HelloServer(int port) throws IOException {
-//    this(port, RouteGuideUtil.getDefaultFeaturesFile());
-//  }
-//
-//  /** Create a RouteGuide server listening on {@code port} using {@code featureFile} database. */
-//  public HelloServer(int port, URL featureFile) throws IOException {
-//    this(ServerBuilder.forPort(port), port, RouteGuideUtil.parseFeatures(featureFile));
-//  }
-
-  /** Create a RouteGuide server using serverBuilder as a base and features as data. */
   public HelloServer(ServerBuilder<?> serverBuilder, int port) {
     this.port = port;
-    this.server = null;
-//    server = serverBuilder.addService(RouteGuideGrpc.bindService(new RouteGuideService(features)))
-//        .build();
+    this.server = serverBuilder.addService(HelloServiceGrpc.bindService(new HelloService())).build();
+  }
+
+  public HelloServer(int port) {
+    this(ServerBuilder.forPort(port), port);
+  }
+
+
+  /** Start serving requests. */
+  public void start() throws IOException {
+    server.start();
+    log.info("Server started, listening on {}", port);
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+
+      @Override
+      public void run() {
+        // Use stderr here since the logger may has been reset by its JVM shutdown hook.
+        System.err.println("*** shutting down gRPC server since JVM is shutting down");
+        HelloServer.this.stop();
+        System.err.println("*** server shut down");
+      }
+    });
+  }
+
+  /** Stop serving requests and shutdown resources. */
+  public void stop() {
+    if (server != null) {
+      server.shutdown();
+    }
+  }
+
+  /**
+   * Await termination on the main thread since the grpc library uses daemon threads.
+   */
+  private void blockUntilShutdown() throws InterruptedException {
+    if (server != null) {
+      server.awaitTermination();
+    }
+  }
+
+  /**
+   * Entry point.
+   */
+  public static void main(String[] args) throws Exception {
+    HelloServer server = new HelloServer(8081);
+    server.start();
+    server.blockUntilShutdown();
+  }
+
+
+  //
+  // HelloService
+  //
+
+  private static final class HelloService implements HelloServiceGrpc.HelloService {
+
+    @Override
+    public void getGreeting(HelloModel.GreetingRequest request, StreamObserver<HelloModel.GreetingReply> responseObserver) {
+      final String name = request.getName();
+      final String greeting = "Hello, " + name + " from GRPC server! Time=" + new Date();
+
+      responseObserver.onNext(HelloModel.GreetingReply.newBuilder().setGreeting(greeting).build());
+      responseObserver.onCompleted();
+    }
   }
 }
